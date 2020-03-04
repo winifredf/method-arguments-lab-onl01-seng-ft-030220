b22e87c00ce0487cb07843d1ae06e6af82cd59dc
Winifred Frierson <winifred.dykes@hotmail.com>
2020-03-04 04:17:18 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,4 +5,7 @@ end
 introduction("Josh")
 
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and "
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+end
+
+introduction_with_language("Dan", "Ember.js")
